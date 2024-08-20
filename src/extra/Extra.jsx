dab22a648e6b34c243fed06f2e16a2c9d410355d
almas hossain <almashossain7384@gmail.com>
2024-08-20 09:22:29 +0600

    paragraph add extra file

--- src/extra/Extra.jsx
+++ src/extra/Extra.jsx
@@ -4,6 +4,7 @@ const Extra = () => {
     return (
         <div>
             <h1>the heading of date 19/08/2024</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore pariatur itaque, optio libero facilis repellendus incidunt beatae sequi alias accusantium?</p>
         </div>
     );
 };
