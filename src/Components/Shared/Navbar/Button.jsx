eb58a8a9cf85798e1485ed189303a3769d55ec65
almas hossain <almashossain7384@gmail.com>
2024-10-29 20:00:00 +0600

    update button color

--- src/Components/Shared/Navbar/Button.jsx
+++ src/Components/Shared/Navbar/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({text}) => {
         <div>
             <button className="text-base font-semibold lg:mx-3 py-2 md:py-0 group relative w-max">
                 <span>{text}</span>
-                <span className="absolute -bottom-1 left-0 w-0 transition-all h-0.5 bg-primary group-hover:w-full"></span>
+                <span className="absolute -bottom-1 left-0 w-0 transition-all h-0.5 bg-red-500 group-hover:w-full"></span>
             </button>
         </div>
     );
